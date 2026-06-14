9bda9540e46e777785c913f193b14513ffbe3c4a
avnamer <avnamer@gmail.com>
2026-06-14 21:23:54 +0300

    feat: add isWatchlist to Folder and targetFolderId to Holding

---
+++ prisma/migrations/20260614000000_watchlist/migration.sql
@@ -0,0 +1,8 @@
+﻿-- AlterTable
+ALTER TABLE "folders" ADD COLUMN "is_watchlist" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "holdings" ADD COLUMN "target_folder_id" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "holdings" ADD CONSTRAINT "holdings_target_folder_id_fkey" FOREIGN KEY ("target_folder_id") REFERENCES "folders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
